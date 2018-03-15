720ef8f79a13f5802ad5c60f312bb18d38fa7fbd
charleserious <wujinjing25@gmail.com>
2018-03-15 16:51:51 +0800

    Two Sum practices

---
+++ JavaScript/Algorithms/TwoSum/TwoSum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+ var twoSum = function (nums, target) {
+    for (var i = 0; i < nums.length, i++;) {
+        
+        for (var j = 1; j < nums.length; j++;) {
+
+            if( i !== j && target === nums[ i ] + nums[ j ]) {
+                return [i, j];
+            }
+
+        }
+        
+    }
+    throw new Error("InvalidArgumentException");
+ };
