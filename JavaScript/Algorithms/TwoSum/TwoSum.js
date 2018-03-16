a3aa52c528fe98ee69d4277894b813efa03c6997
charleserious <wujinjing25@gmail.com>
2018-03-16 12:55:07 +0800

    format code

--- JavaScript/Algorithms/TwoSum/TwoSum.js
+++ JavaScript/Algorithms/TwoSum/TwoSum.js
@@ -7,7 +7,7 @@
  var twoSum = function (nums, target) {
     for (var i = 0; i < nums.length, i++;) {
         
-        for (var j = 1; j < nums.length; j++;) {
+        for (var j = 1; j < nums.length; j++) {
 
             if( i !== j && target === nums[ i ] + nums[ j ]) {
                 return [i, j];
