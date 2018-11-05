f464ce3c1a29cf30626cf6edb4c1ffcc46516586
charleserious <wujinjing25@gmail.com>
2018-11-05 15:13:53 +0800

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"commonjs": true,
+		"es6": true,
+		"node": true
+	},
+	"extends": "eslint-config-semistandard",
+	"parserOptions": {
+		"ecmaVersion": 2016,
+		"sourceType": "module"
+	}
+};
