857b5381f5fe1698e014da50ceab33056104cee5
wujinjing <wujinjing@kedacom.com>
2019-03-15 12:56:23 +0800

    122. Best Time to Buy and Sell Stock II

---
+++ JavaScript/Algorithms/122.best-time-to-buy-and-sell-stock-ii/index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {Array} prices
+ * @returns {Number}
+ */
+const maxProfit = (prices) => {
+
+
+const prices = [7, 1, 5, 3, 6, 4];
+// const prices = [2, 4, 1];
+const profit = maxProfit(prices);
+console.log(profit);
