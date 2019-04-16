2ba8ff8b49bef6190168e87f57f6fec44c48d191
wujinjing <wujinjing@kedacom.com>
2019-04-16 12:14:40 +0800

    comment output

--- JavaScript/Algorithms/66.plus-one/index.js
+++ JavaScript/Algorithms/66.plus-one/index.js
@@ -13,4 +13,4 @@ const plusOne = (digits) => {
 
 const digits = [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3];
 const added = plusOne(digits);
-console.log(JSON.stringify(added));
+console.log(JSON.stringify(added)); // [6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,4]
