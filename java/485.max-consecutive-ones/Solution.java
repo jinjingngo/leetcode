3a615043a3f40ed3dbd27b059aafcf34c43c22a0
charleserious <wujinjing25@gmail.com>
2020-05-24 15:52:14 +0800

    fulfilled

--- java/485.max-consecutive-ones/Solution.java
+++ java/485.max-consecutive-ones/Solution.java
@@ -1,11 +1,23 @@
+import java.util.Arrays;
+
 class Solution {
   public static int findMaxConsecutiveOnes(int[] nums) {
-    return 0;
+    int sum = 0;
+    int max = 0;
+    for (int i : nums) {
+      sum += i;
+      if (i == 0) {
+        sum = 0;
+      } else {
+        max = Math.max(max, sum);
+      }
+    }
+    return max;
   }
 
   public static void main(String[] args) {
     int[] nums = { 1, 1, 0, 1, 1, 1 };
     int result = findMaxConsecutiveOnes(nums);
-    System.out.println(result);
+    System.out.println(result); // 3
   }
 }
