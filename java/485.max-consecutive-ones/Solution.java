2484aa5b6ecdf52f338e757f6a36f164bddd908a
charleserious <wujinjing25@gmail.com>
2020-05-24 14:55:37 +0800

    move out of the directory

---
+++ java/485.max-consecutive-ones/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+  public static int findMaxConsecutiveOnes(int[] nums) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { 1, 1, 0, 1, 1, 1 };
+    int result = findMaxConsecutiveOnes(nums);
+    System.out.println(result);
+  }
+}
