861b7836bdbe29ad3abc0a8b33576185fd9d5735
wujinjing <wujinjing@kedacom.com>
2020-06-17 13:00:28 +0800

    rename file

--- algorithms/java/1.two-sum/Solution.java
+++ algorithms/java/1.two-sum/Solution.java
@@ -1,13 +1,11 @@
 import java.util.HashMap;
 import java.util.Map;
 
-import org.graalvm.compiler.lir.amd64.AMD64Binary.TwoOp;
-
 class Solution {
   public static int[] twoSum(int[] numbers, int target) {
     int[] result = new int[2];
     Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    for(int i = 0; i < numbers.length; i++) {
+    for (int i = 0; i < numbers.length; i++) {
       int complement = target - numbers[i];
       if (map.containsKey(complement)) {
         result[1] = i;
@@ -18,7 +16,7 @@ class Solution {
     return result;
   }
 
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     int[] numbers = { 2, 7, 11, 15 };
     int target = 9;
     int[] result = twoSum(numbers, target);
