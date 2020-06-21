dfca2af7247046faecca24d2a3c86475cd1a79b7
charleserious <wujinjing25@gmail.com>
2020-06-21 23:41:06 +0800

    problem description

--- algorithms/java/src/twoSum/TwoSum.java
+++ algorithms/java/src/twoSum/TwoSum.java
@@ -1,3 +1,18 @@
+// Source: https://leetcode.com/problems/two-sum/
+
+/******************************************************
+ * 
+ * Given an array of integers, return **indices** of the two numbers such that they add up to a 
+ * specific target.
+ * 
+ * You may assume that each input whould have *exactly* one solution, and you may not use the 
+ * *same* element twice
+ * 
+ * Given nums: [2, 7, 11, 15], target: 9.
+ * 
+ * Should return [0, 1], because nums[0] + num[1] = 2 + 7 = 9. 
+ * 
+ ******************************************************/
 // package twoSum;
 
 import java.util.HashMap;
