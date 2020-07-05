579e7bbbc4d70e7cf578bbb29657c1ccbb681824
wujinjing <wujinjing@kedacom.com>
2020-07-05 17:28:46 +0800

    Longest Increasing Subsequence

---
+++ algorithms/java/src/longestIncreasingSubsequence/LongestIncreasingSubsequence.java
@@ -0,0 +1,22 @@
+// Source: https://leetcode.com/problems/longest-increasing-subsequence/
+
+/**
+ *
+ * Given an unsorted array of integers, find the length of longest increasing subsequence.
+ *
+ * The Longest Increasing Subsequence (LIS) problem is to find the length of a given subsequence
+ * such that all elements of the subsequence are sorted in increasing order.
+ *
+ * For example,
+ * the length LIS for {10, 22, 9, 33, 21, 50, 41, 60, 80}
+ * is 6
+ * and LIS is {10, 22, 33, 50, 60, 80}
+ *
+ */
+package longestIncreasingSubsequence;
+
+public class LongestIncreasingSubsequence {
+  public int lengthOfLIS(int[] nums) {
+    return 0;
+  }
+}
