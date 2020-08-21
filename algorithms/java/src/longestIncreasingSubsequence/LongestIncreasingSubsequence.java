6aa03529e9123b10d68ff7eb8d62391cff0625be
wujinjing <wujinjing@kedacom.com>
2020-08-21 12:59:06 +0800

    Longest Increasing Subsequence

--- algorithms/java/src/longestIncreasingSubsequence/LongestIncreasingSubsequence.java
+++ algorithms/java/src/longestIncreasingSubsequence/LongestIncreasingSubsequence.java
@@ -12,11 +12,29 @@
  * is 6
  * and LIS is {10, 22, 33, 50, 60, 80}
  *
+ * Note: Lowest to highest, and in which the subsequence is as long as possible,
+ * the subsequence is not necessarily contiguous, or unique, duplicate numbers are not counted as increasing subsequence.
+ *
  */
 package longestIncreasingSubsequence;
 
 public class LongestIncreasingSubsequence {
   public int lengthOfLIS(int[] nums) {
-    return 0;
+    int[] tails = new int[nums.length];
+    int size = 0;
+    for (int x : nums) {
+      int i = 0, j = size;
+      while (i != j) {
+        int m = (i + j) / 2;
+        if (tails[m] < x) {
+          i = m + 1;
+        } else {
+          j = m;
+        }
+      }
+      tails[i] = x;
+      if (i == size) ++size;
+    }
+    return size;
   }
 }
--- algorithms/java/src/longestIncreasingSubsequence/TestLongestIncreaingSubsequence.java
+++ algorithms/java/src/longestIncreasingSubsequence/TestLongestIncreaingSubsequence.java
@@ -1,5 +1,15 @@
 package longestIncreasingSubsequence;
 
-public class TestLongestIncreaingSubsequence {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestLongestIncreaingSubsequence {
+  @Test
+  public void testLengthOfLIS() {
+    LongestIncreasingSubsequence longestIncreasingSubsequence = new LongestIncreasingSubsequence();
+    int[] nums = { 10, 22, 9, 33, 21, 50, 41, 60, 80 };
+    int sorted = longestIncreasingSubsequence.lengthOfLIS(nums);
+    int except = 6;
+    Assert.assertEquals(except, sorted);
+  }
 }
